6b0f2565b9378531ac39d030cca1dfe7e3b9f575
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-05-22 14:50:54 +0800

    done preparing the tailwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,17 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
+        @vite(['resources/css/app.css'])
         
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div>
             Hello World
         </div>
+        
+        
     </body>
 </html>
