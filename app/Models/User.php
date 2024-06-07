7b911b3581ede094b6cc317744bacc3ccf23bf25
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-06-07 19:38:47 +0800

    Done auth user, admin, and business_admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'email',
         'password',
         'user_type',
-    ];
+    ]; 
 
     /**
      * The attributes that should be hidden for serialization.
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Auth;
+
 
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login.create');
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('login.store');
 
-Route::get('/admin', function () {
-    return view('admin.index');
-})->name('admin.index');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login.create');
+
+//need to be login before can access to the user page, admin page and business_admin page
+Route::middleware(['auth'])->group(function () {
+    Route::get('/user', function () {
+        return view('user.index');
+    })->name('user.index');
+
+    Route::get('/admin', function () {
+        return view('admin.index');
+    })->name('admin.index');
+
+    Route::get('/business_admin', function () {
+        return view('business_admin.index');
+    })->name('business_admin.index');
+});
+
+// Route::get('/register', function () {
+//     return view('auth.register');
+// })->name('login.store');
+
+// Route::get('/admin', function () {
+//     return view('admin.index');
+// })->name('admin.index');
