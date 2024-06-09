2798764ed018cc8f4197045a8c4cfdc814357a09
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-06-09 14:27:38 +0800

    Done preparing the business salon

--- app/Http/Controllers/Admin/SalonController.php
+++ app/Http/Controllers/Admin/SalonController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Business;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class SalonController extends Controller
 {
     //
     public function index(){
-        return view('admin.salon_list');
+    
+
+        $pendingSalon = Business::with('user')->where('status','pending')->get();
+        $approvedSalon = Business::with('user')->where('status','approved')->get();
+
+        return view('admin.salon_list',[
+            'pendingSalon' => $pendingSalon,
+            'approvedSalon' => $approvedSalon
+        ]);
     }
 }
--- resources/views/admin/salon_list.blade.php
+++ resources/views/admin/salon_list.blade.php
@@ -6,15 +6,111 @@
             </x-aside>
         </div>
         <main class="ml-17rem w-full">
-            <h1 class="text-2xl font-semibold mt-3">Salon</h1>
-            <div class="bg-[#fff] p-[2rem] border w-[97%] rounded-md hover:shadow-xl transition-all mx-auto">
-                
+            <div>
+                <h1 class="text-2xl font-semibold mt-3">Pending Business Salon</h1>
+                <div class="bg-[#fff] p-[2rem] border w-[97%] rounded-md hover:shadow-xl transition-all mx-auto">
+                    <div class="table_container">
+                        <x-table.table id="myTable">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <x-table.thead>
+                                        Business Name
+                                    </x-table.thead>
+                                    <x-table.thead>
+                                        Owner
+                                    </x-table.thead>
+                                    <x-table.thead>
+                                        Address
+                                    </x-table.thead>
+                                    <x-table.thead>
+                                        Contact Info
+                                    </x-table.thead>
+                                    <x-table.thead>
+                                        Action
+                                    </x-table.thead>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($pendingSalon as $salon)
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <x-table.td>
+                                        {{$salon->business_name}}
+                                    </x-table.td>
+                                    <x-table.td>
+                                        {{$salon->user->first_name.' '.$salon->user->last_name}}
+                                    </x-table.td>
+                                    <x-table.td>
+                                        {{$salon->address}}
+                                    </x-table.td>
+                                        <x-table.td>
+                                        {{$salon->contact_info}}
+                                    </x-table.td>
+                                    <x-table.td>
+                                        <x-table.button-action>edit</x-table.button-action>
+                                    </x-table.td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </x-table.table>
+                    </div>
+                </div>
             </div>
+            <div class="mt-20">
+                <h1 class="text-2xl font-semibold mt-3">Aprroved Business Salon</h1>
+                <div class="bg-[#fff] p-[2rem] border w-[97%] rounded-md hover:shadow-xl transition-all mx-auto">
+                    <div class="table_container">
+                        <x-table.table id="myTable2">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <x-table.thead>
+                                        Business Name
+                                    </x-table.thead>
+                                    <x-table.thead>
+                                        Owner
+                                    </x-table.thead>
+                                    <x-table.thead>
+                                        Address
+                                    </x-table.thead>
+                                    <x-table.thead>
+                                        Contact Info
+                                    </x-table.thead>
+                                    <x-table.thead>
+                                        Action
+                                    </x-table.thead>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($approvedSalon as $salon)
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <x-table.td>
+                                        {{$salon->business_name}}
+                                    </x-table.td>
+                                    <x-table.td>
+                                        {{$salon->user->first_name.' '.$salon->user->last_name}}
+                                    </x-table.td>
+                                    <x-table.td>
+                                        {{$salon->address}}
+                                    </x-table.td>
+                                        <x-table.td>
+                                        {{$salon->contact_info}}
+                                    </x-table.td>
+                                    <x-table.td>
+                                        <x-table.button-action>edit</x-table.button-action>
+                                    </x-table.td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </x-table.table>
+                    </div>
+                </div>
+            </div>
+            
         </main>
     </div>
     <script>
         $(document).ready( function () {
             $('#myTable').DataTable();
+            $('#myTable2').DataTable();
         } );
     </script>
 </x-layout>
