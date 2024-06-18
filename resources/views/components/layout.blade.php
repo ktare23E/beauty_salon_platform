e3423d48250feb1f1fba82bfe659a82961826ded
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-06-18 20:32:55 +0800

    favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,8 @@
         <link rel="stylesheet" href="https://cdn.datatables.net/2.0.7/css/dataTables.dataTables.css" />
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
+        <link rel="icon" type="image/x-icon" href="{{asset('imgs/salon logo.jpg')}}">
+
 
         {{-- fonts --}}
         <link rel="preconnect" href="https://fonts.googleapis.com">
