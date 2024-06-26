efbef5f9a0f48829f39e9ac7a1055e859ef158b4
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-06-27 06:54:42 +0800

    Done admin dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Business;
+use App\Models\Requirement;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
@@ -15,6 +18,20 @@ class DashboardController extends Controller
         if($user->user_type != 'admin'){
             abort('403');
         }
-        return view('admin.index');
+        $requirements = Requirement::where('status','active')->get();
+        $businessAdminCount = User::where('user_type','business_admin')->count();
+        $clientCount = User::where('user_type','client')->count();
+        $approvedBusinessCount = Business::where('status','approved')->count();
+        $pendingBusinessCount = Business::where('status','pending')->count();
+        $latestBusinessApproved = Business::where('status','approved')->orderBy('id','desc')->limit(1)->get();
+
+        return view('admin.index',[
+            'requirements' => $requirements,
+            'businessAdminCount' => $businessAdminCount,
+            'clientCount' => $clientCount,
+            'approvedBusinessCount' => $approvedBusinessCount,
+            'pendingBusinessCount' => $pendingBusinessCount,
+            'latestBusinessApproved' => $latestBusinessApproved
+        ]);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,19 +2,52 @@
     <div class="w-full h-full grid grid-cols-[15%,1fr] gap-2 bg-[#f6f6f9]">
         <div class=" flex h-screen rounded-tr-md">
             <x-aside>
-
+                
             </x-aside>
         </div>
-        <main class="ml-17rem w-full">
-            <h1 class="text-2xl font-semibold mt-3">Dashboard</h1>
-            <div class="bg-[#fff] p-[2rem] border w-[97%] rounded-md hover:shadow-xl transition-all mx-auto">
-                
+        <main class="w-full">
+            <h1 class="text-2xl font-semibold mt-3">Admin Dashboard</h1>
+            <div class="analytics mt-12 w-[90%] mx-auto">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div class="bg-white p-4 rounded-md shadow-md">
+                        <h2 class="text-xl font-semibold">Active Requirements</h2>
+                        @forelse ($requirements as $requirement)
+                            <p class="text-xl ">{{$requirement->requirement_name}}</p>
+                        @empty
+                            <p class="text-xl ">No requirements yet.</p>
+                        @endforelse
+                        <a href="" class="mt-2 text-blue-500 text-sm hover:underline">view details</a>
+                    </div>
+                    <div class="bg-white p-4 rounded-md shadow-md flex flex-col justify-between">
+                        <h2 class="text-xl font-semibold">Total Salon Owner</h2>
+                        <p class="text-3xl font-semibold">{{$businessAdminCount}}</p>
+                        <a href="" class="mt-2 text-blue-500 text-sm hover:underline">view details</a>
+                    </div>
+                    <div class="bg-white p-4 rounded-md shadow-md flex flex-col justify-between">
+                        <h2 class="text-xl font-semibold">Total Client</h2>
+                        <p class="text-3xl font-semibold">{{$clientCount}}</p>
+                        <a href="" class="mt-2 text-blue-500 text-sm hover:underline">view details</a>
+                    </div>
+                    <div class="bg-white p-4 rounded-md shadow-md flex flex-col justify-between">
+                        <h2 class="text-xl font-semibold">Total Approved Salon</h2>
+                        <p class="text-3xl font-semibold">{{$approvedBusinessCount}}</p>
+                        <a href="" class="mt-2 text-blue-500 text-sm hover:underline">view details</a>
+                    </div>
+                    <div class="bg-white p-4 rounded-md shadow-md flex flex-col justify-between">
+                        <h2 class="text-xl font-semibold">Total Pending Salon</h2>
+                        <p class="text-3xl font-semibold">{{$pendingBusinessCount}}</p>
+                        <a href="" class="mt-2 text-blue-500 text-sm hover:underline">view details</a>
+                    </div>
+                    <div class="bg-white p-4 rounded-md shadow-md flex flex-col justify-between">
+                        <h2 class="text-xl font-semibold">Latest Approved Salon</h2>
+                        <p class="text-3xl font-semibold">{{$latestBusinessApproved[0]->business_name}}</p>
+                        <a href="" class="mt-2 text-blue-500 text-sm hover:underline">view details</a>
+                    </div>
+                </div>
             </div>
         </main>
     </div>
     <script>
-        $(document).ready( function () {
-            $('#myTable').DataTable();
-        } );
+        
     </script>
 </x-layout>
