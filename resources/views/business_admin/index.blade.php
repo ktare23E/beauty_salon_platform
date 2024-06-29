05ae9a96971b67808bc6c3d55ff0bdec3dc0eb79
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-06-29 22:42:05 +0800

    view details salon dashboard

--- resources/views/business_admin/index.blade.php
+++ resources/views/business_admin/index.blade.php
@@ -9,31 +9,32 @@
             <h1 class="text-2xl font-semibold mt-3">Business Admin Dashboard</h1>
             <div class="analytics mt-12 w-[90%] mx-auto">
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <div class="bg-white p-4 rounded-md shadow-md">
+                    <div class="bg-white p-4 rounded-md transition hover:shadow-2xl">
                         <h2 class="text-xl font-semibold">Business Owner</h2>
                         <p class="text-xl">-{{$user->first_name.' '.$user->last_name}}</p>
                     </div>
-                    <div class="bg-white p-4 rounded-md shadow-md">
+                    <div class="bg-white p-4 rounded-md transition hover:shadow-2xl">
                         <h2 class="text-xl font-semibold">Business Salon's</h2>
                         @forelse ($businesses as $business)
                             <p class="text-xl">- {{$business->business_name}}</p>
                         @empty
                             <p class="text-3xl font-semibold">No business salon yet.</p>
                         @endforelse
+                        <a href="{{route('business_admin.salon')}}" class="text-blue-500 text-sm hover:underline">view details</a>
                     </div>
-                    <div class="bg-white p-4 rounded-md shadow-md">
+                    <div class="bg-white p-4 rounded-md transition hover:shadow-2xl">
                         <h2 class="text-xl font-semibold">Total Products</h2>
                         <p class="text-3xl font-semibold">100</p>
                     </div>
-                    <div class="bg-white p-4 rounded-md shadow-md">
+                    <div class="bg-white p-4 rounded-md transition hover:shadow-2xl">
                         <h2 class="text-xl font-semibold">Total Products</h2>
                         <p class="text-3xl font-semibold">100</p>
                     </div>
-                    <div class="bg-white p-4 rounded-md shadow-md">
+                    <div class="bg-white p-4 rounded-md transition hover:shadow-2xl">
                         <h2 class="text-xl font-semibold">Total Products</h2>
                         <p class="text-3xl font-semibold">100</p>
                     </div>
-                    <div class="bg-white p-4 rounded-md shadow-md">
+                    <div class="bg-white p-4 rounded-md transition hover:shadow-2xl">
                         <h2 class="text-xl font-semibold">Total Products</h2>
                         <p class="text-3xl font-semibold">100</p>
                     </div>
